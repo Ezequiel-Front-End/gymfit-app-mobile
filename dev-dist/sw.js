8da76fcbb6271f28740e75e48d1ea0069de3731a
Ezequiel Fernandes <ezequiel.lobo@abctechnology.com.br>
2026-09-13 01:49:14 -0300

    feat: melhorando os filtros.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-b1bafff1'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r629jvd4mf8"
+    "revision": "0.3cnlr8ankl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
